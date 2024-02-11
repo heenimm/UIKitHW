5fd3ed02e421d9d637e4f364880feb1bc7207769
asmagomedova <m.h.axm@mail.ru>
2024-02-11 22:57:37 +0300

    add color set and image set

---
+++ UIKitHW/UIKitHW/PlayerViewController.swift
@@ -0,0 +1,11 @@
+// PlayerViewController.swift
+// Copyright © RoadMap. All rights reserved.
+
+import UIKit
+
+class PlayerViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor(named: "AppPurple")
+    }
+}
